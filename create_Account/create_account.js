8d885e4440e3311db740364ab9c676ba8a8d431d
Jiawei Zhu <vortca@Jiaweis-MacBook-Pro.local>
2020-09-05 19:30:57 +0800

    Third Commit

--- create_Account/create_account.js
+++ create_Account/create_account.js
@@ -6,33 +6,32 @@ function validate() {
     var confirm_pword = document.getElementById("confirm_password");
     var form = document.getElementById("submit_form");
 
-    form.addEventListener('submit',(e)=> {
     if (reg.test(address) == false)
     {
         alert('请填写正确的邮箱信息');
     }
-    if (address.value == "")
+    else if (address.value == "")
     { 
         alert('请填写正确的邮箱信息');
     }
-    if (uname.value == "")
+    else if (uname.value == "")
     {
         alert('请填写正确的姓名信息');
     }
-    if (pword.value == "")
+    else if (pword.value == "")
     {
         alert('请填写密码');
     }
-    if (confirm_pword.value == "") 
+    else if (confirm_pword.value == "") 
     {
         alert('请再次输入密码');
     }
     else if(username.value == "admin" && password.value == "123456")
     {
         window.location.href="personal_info.html";
-    } else 
+    } 
+    else 
     {
         alert("请输入正确的用户名和密码")
     }
-})
 }
