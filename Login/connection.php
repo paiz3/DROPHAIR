5362597728fe113aaa245a9f489b0a003a4fc30c
AaronGuoZY <zaiyangguo@gmail.com>
2020-09-06 10:34:11 +0800

    added test folder and file

--- Login/connection.php
+++ Login/connection.php
@@ -8,4 +8,5 @@ $con = mysqli_connect($host, $user, $password, $db_name);
 if(mysqli_connect_errno()) {
     die("Failed to connect with MySQL: ". mysqli_connect_error());
 }
+//This is a test comment
 ?>
